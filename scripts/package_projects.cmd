0107306a0436aaadcc1484a00aa6ec34ae6b7eab
Stephen Foster <stephenfostercpp@gmail.com>
2022-09-13 00:21:26 -0700

    Roll a ball 1.0.0

--- scripts/package_projects.cmd
+++ scripts/package_projects.cmd
@@ -2,7 +2,7 @@
 setlocal
 set scripts=%~dp0
 set tools=%~dp0..\tools
-echo ...packaging binaries
+echo ...packaging projects
 mkdir %scripts%..\assignments\
-for /d %%a in (%scripts%..\projects\*) do ( "%tools%\7-Zip\7z.exe" a -tzip "%scripts%..\assignments\%%~na" "%%a" -mx5
+for /d %%a in (%scripts%..\projects\*) do ( "%tools%\7-Zip\7z.exe" a -tzip "%scripts%..\assignments\%%~na-Stephen-Foster" "%%a" -mx5 > "%scripts%..\assignments\%%~na-package-log.txt" 
 )
